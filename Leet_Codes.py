808425e56609b0a11fae0c2d74fd15920457acbf
Pardhu Kadali <114857356+PardhuKadali@users.noreply.github.com>
2023-05-30 15:20:44 +0530

    Update Leet_Codes.py

--- Leet_Codes.py
+++ Leet_Codes.py
@@ -239,7 +239,7 @@ class Leetcode:
 
 
 
-def lc_1119(self,x,y):
+    def lc_1119(self,x,y):
     
         '''
         Given a string S, remove the vowels 'a', 'e', 'i', 'o', and 'u' from it, and return the new string.
